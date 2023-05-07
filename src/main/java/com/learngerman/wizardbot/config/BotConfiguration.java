3c0baf7a73822be4a3aca72ffae78d369ef5d2d3
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-07 08:25:41 +0200

    Initial commit

---
+++ src/main/java/com/learngerman/wizardbot/config/BotConfiguration.java
@@ -0,0 +1,36 @@
+package com.learngerman.wizardbot.config;
+
+import com.learngerman.wizardbot.event.EventListener;
+import discord4j.core.DiscordClientBuilder;
+import discord4j.core.GatewayDiscordClient;
+import discord4j.core.event.domain.Event;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class BotConfiguration {
+
+    @Value("${wizard-token}")
+    private String wizardToken;
+
+    @Bean
+    public <T extends Event> GatewayDiscordClient gatewayDiscordClient(List<EventListener<T>> eventListeners) {
+        GatewayDiscordClient client = DiscordClientBuilder.create(wizardToken)
+                .build()
+                .login()
+                .block();
+
+        for(EventListener<T> listener : eventListeners) {
+            client.on(listener.getEventType())
+                    .flatMap(listener::execute)
+                    .onErrorResume(listener::handleError)
+                    .subscribe();
+        }
+
+        return client;
+    }
+
+}
---
+++ src/test/java/com/learngerman/wizardbot/WizardBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learngerman.wizardbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
