013cee546c79439edc188dcda96c800f75140e4f
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-09 17:17:09 +0200

    Student api

---
+++ src/main/java/com/learngerman/wizardbot/student/StudentRowMapper.java
@@ -0,0 +1,19 @@
+package com.learngerman.wizardbot.student;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.OffsetDateTime;
+
+public class StudentRowMapper implements RowMapper<Student> {
+    @Override
+    public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new Student(
+          rs.getLong("d_uid"),
+          rs.getFloat("gold_balance"),
+          rs.getBoolean("is_engaged"),
+                (OffsetDateTime) rs.getObject("balance_defrost_date")
+        );
+    }
+}
