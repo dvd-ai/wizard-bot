0351da30f5ccd215684f7d9daafd5ae81ddb49c6
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-11 16:51:52 +0200

    buildUserInfoMessage

--- src/main/java/com/learngerman/wizardbot/util/ResponseMessageBuilder.java
+++ src/main/java/com/learngerman/wizardbot/util/ResponseMessageBuilder.java
@@ -1,8 +1,13 @@
 package com.learngerman.wizardbot.util;
 
+import com.learngerman.wizardbot.command.MemberInfo;
+import discord4j.core.spec.EmbedCreateFields;
 import discord4j.core.spec.EmbedCreateSpec;
 import discord4j.core.spec.MessageCreateSpec;
 import discord4j.rest.util.Color;
+import reactor.core.publisher.Mono;
+
+import java.time.Instant;
 
 public class ResponseMessageBuilder {
 
@@ -18,5 +23,27 @@ public class ResponseMessageBuilder {
                         .build()
                 ).build()
         ;
+    }
+
+    public static EmbedCreateSpec buildUserInfoMessage(String title, String description, String memberAvatarLink) {
+
+        return EmbedCreateSpec.builder()
+                .color(Color.VIVID_VIOLET)
+                .title(title)
+                .thumbnail(memberAvatarLink)
+                .description(description)
+                .build();
+
+
+//        return MessageCreateSpec.builder()
+//                .addEmbed(EmbedCreateSpec.builder()
+//                        .color(Color.VIVID_VIOLET)
+//                        .title(title)
+//                        .description( description)
+//                        .author(EmbedCreateFields.Author.of("@" + title, null, avatarLink))
+//                        .image(avatarLink)
+//                        .build()
+//                ).build()
+
     }
 }
