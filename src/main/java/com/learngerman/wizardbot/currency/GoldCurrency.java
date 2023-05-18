944ef0dabbf108e2ece58124abbdeaa0f967f51b
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-18 12:31:43 +0200

    enum with gold values

---
+++ src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
@@ -0,0 +1,10 @@
+package com.learngerman.wizardbot.currency;
+
+public class GoldCurrency {
+    public static final float GOLD_START_CAPITAL = 25f;
+    public static final float GOLD_MESSAGE_REWARD = 0.05f;
+
+    private GoldCurrency() {
+    }
+}
+
