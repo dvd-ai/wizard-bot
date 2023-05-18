3e1ca6309b7ee0b1a5eb21fd4d13eda09e4d2803
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-18 21:05:21 +0200

    constant usage, ignore exception raised, when there is no user in db, when the user sends the automatic hello-message

--- src/main/java/com/learngerman/wizardbot/simple_message/SimpleMessageManager.java
+++ src/main/java/com/learngerman/wizardbot/simple_message/SimpleMessageManager.java
@@ -5,17 +5,21 @@ import discord4j.core.object.entity.Message;
 import org.springframework.stereotype.Component;
 
 import static com.learngerman.wizardbot.command.MessageEventUtils.getMessageAuthorDiscordId;
+import static com.learngerman.wizardbot.currency.GoldCurrency.GOLD_MESSAGE_REWARD;
 
 @Component
 public class SimpleMessageManager {
     private final StudentService studentService;
-    private final float MESSAGE_REWARD = 0.5f;
 
     public SimpleMessageManager(StudentService studentService) {
         this.studentService = studentService;
     }
 
     public void process(Message message) {
-        studentService.increaseStudentGoldCurrencyBy(MESSAGE_REWARD, getMessageAuthorDiscordId(message));
+        try {
+            studentService.increaseStudentGoldCurrencyBy(GOLD_MESSAGE_REWARD, getMessageAuthorDiscordId(message));
+        } catch (RuntimeException e) {
+
+        }
     }
 }
