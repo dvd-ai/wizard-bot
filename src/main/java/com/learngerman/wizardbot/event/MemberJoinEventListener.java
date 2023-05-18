4da0267db314bb193f527205e875ae04981dad72
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-18 12:33:48 +0200

    MemberJoinEventListener

---
+++ src/main/java/com/learngerman/wizardbot/event/MemberJoinEventListener.java
@@ -0,0 +1,28 @@
+package com.learngerman.wizardbot.event;
+
+import com.learngerman.wizardbot.member_join.MemberJoinManager;
+import discord4j.core.event.domain.guild.MemberJoinEvent;
+import reactor.core.publisher.Mono;
+
+public class MemberJoinEventListener implements EventListener<MemberJoinEvent>{
+    private final MemberJoinManager memberJoinManager;
+
+    public MemberJoinEventListener(MemberJoinManager memberJoinManager) {
+        this.memberJoinManager = memberJoinManager;
+    }
+
+    @Override
+    public Class<MemberJoinEvent> getEventType() {
+        return MemberJoinEvent.class;
+    }
+
+    @Override
+    public Mono<Void> execute(MemberJoinEvent event) {
+        event.getMember().asFullMember()
+                .filter(member -> !member.isBot())
+                .doOnNext(memberJoinManager::process)
+                .subscribe()
+        ;
+        return Mono.empty();
+    }
+}
