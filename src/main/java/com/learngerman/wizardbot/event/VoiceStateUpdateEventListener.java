4cf20cb831d391fd8429264cdd246aa1edcfa5d2
Daniil Denisov <56834006+dvd-ai@users.noreply.github.com>
2023-05-20 09:38:55 +0200

    25 grant gold vc (#39)
    * GOLD_VOICE_REWARD
    
    * voice chat manager
    
    * VoiceStateUpdateEventListener

---
+++ src/main/java/com/learngerman/wizardbot/event/VoiceStateUpdateEventListener.java
@@ -0,0 +1,27 @@
+package com.learngerman.wizardbot.event;
+
+import com.learngerman.wizardbot.voice_chat.VoiceChatManager;
+import discord4j.core.event.domain.VoiceStateUpdateEvent;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+
+@Service
+public class VoiceStateUpdateEventListener implements EventListener<VoiceStateUpdateEvent>{
+    private final VoiceChatManager voiceChatManager;
+
+    public VoiceStateUpdateEventListener(VoiceChatManager voiceChatManager) {
+        this.voiceChatManager = voiceChatManager;
+    }
+
+    @Override
+    public Class<VoiceStateUpdateEvent> getEventType() {
+        return VoiceStateUpdateEvent.class;
+    }
+
+    @Override
+    public Mono<Void> execute(VoiceStateUpdateEvent event) {
+        voiceChatManager.process(event);
+        return Mono.empty();
+    }
+}
