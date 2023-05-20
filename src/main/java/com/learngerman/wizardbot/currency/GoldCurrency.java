f8723d84e46d6304f61dcadba510b479790d73ef
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-20 14:02:35 +0200

    gold day tax constant

--- src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
+++ src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
@@ -4,6 +4,7 @@ public class GoldCurrency {
     public static final float GOLD_START_CAPITAL = 25f;
     public static final float GOLD_MESSAGE_REWARD = 0.05f;
     public static final float GOLD_VOICE_REWARD = 0.01f;
+    public static final float GOLD_DAY_TAX = 10f;
 
     private GoldCurrency() {
     }
