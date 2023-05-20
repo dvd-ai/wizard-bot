857ffa365e603d129260ef41dab7a34f5fd59175
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-20 09:33:52 +0200

    GOLD_VOICE_REWARD

--- src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
+++ src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
@@ -3,6 +3,7 @@ package com.learngerman.wizardbot.currency;
 public class GoldCurrency {
     public static final float GOLD_START_CAPITAL = 25f;
     public static final float GOLD_MESSAGE_REWARD = 0.05f;
+    public static final float GOLD_VOICE_REWARD = 0.01f;
 
     private GoldCurrency() {
     }
