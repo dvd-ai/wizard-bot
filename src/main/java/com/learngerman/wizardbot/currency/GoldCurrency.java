6af088488eff6c4c2832cb77be68b1c784418f73
Daniil Denisov <56834006+dvd-ai@users.noreply.github.com>
2023-05-21 21:34:30 +0200

    Message gold accumulating update (#43)
    * new constant and proper naming
    
    * now the grant depends on the amount of the words in the message & the message aren't processed when the channel is ignored

--- src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
+++ src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
@@ -2,8 +2,8 @@ package com.learngerman.wizardbot.currency;
 
 public class GoldCurrency {
     public static final float GOLD_START_CAPITAL = 25f;
-    public static final float GOLD_MESSAGE_REWARD = 0.05f;
-    public static final float GOLD_VOICE_REWARD = 0.01f;
+    public static final float GOLD_MESSAGE_REWARD_PER_WORD = 0.1f;
+    public static final float GOLD_VOICE_REWARD_PER_SECOND = 0.01f;
 
     private GoldCurrency() {
     }
--- src/main/java/com/learngerman/wizardbot/simple_message/SimpleMessageManager.java
+++ src/main/java/com/learngerman/wizardbot/simple_message/SimpleMessageManager.java
@@ -1,25 +1,47 @@
 package com.learngerman.wizardbot.simple_message;
 
+import com.learngerman.wizardbot.channel.Channel;
+import com.learngerman.wizardbot.channel.ChannelService;
 import com.learngerman.wizardbot.student.StudentService;
 import discord4j.core.object.entity.Message;
 import org.springframework.stereotype.Component;
 
 import static com.learngerman.wizardbot.command.MessageEventUtils.getMessageAuthorDiscordId;
-import static com.learngerman.wizardbot.currency.GoldCurrency.GOLD_MESSAGE_REWARD;
+import static com.learngerman.wizardbot.currency.GoldCurrency.GOLD_MESSAGE_REWARD_PER_WORD;
 
 @Component
 public class SimpleMessageManager {
     private final StudentService studentService;
+    private final ChannelService channelService;
 
-    public SimpleMessageManager(StudentService studentService) {
+    public SimpleMessageManager(StudentService studentService, ChannelService channelService) {
         this.studentService = studentService;
+        this.channelService = channelService;
     }
 
     public void process(Message message) {
         try {
-            studentService.increaseStudentGoldCurrencyBy(GOLD_MESSAGE_REWARD, getMessageAuthorDiscordId(message));
+            if (isMessageFromIgnoredChannel(message))
+                return;
+            int wordAmount = calculateWords(message.getContent());
+            studentService.increaseStudentGoldCurrencyBy(wordAmount * GOLD_MESSAGE_REWARD_PER_WORD, getMessageAuthorDiscordId(message));
         } catch (RuntimeException e) {
 
         }
     }
+
+    private int calculateWords(String content) {
+        if (content == null || content.isEmpty()) {
+            return 0;
+        }
+        String[] words = content.split("\\s+");
+        return words.length;
+    }
+
+    private boolean isMessageFromIgnoredChannel(Message message) {
+        Channel usedChannel = channelService.getChannelById(message.getChannelId().asLong());
+        return usedChannel.isIgnoredForCurrencyOperations();
+    }
+
+
 }
