2181a42b1233d58a4f29e888d0606f3de5e75d8a
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-26 08:23:43 +0200

    added: proper getting Date

--- src/main/java/com/learngerman/wizardbot/student/StudentRowMapper.java
+++ src/main/java/com/learngerman/wizardbot/student/StudentRowMapper.java
@@ -2,17 +2,25 @@ package com.learngerman.wizardbot.student;
 
 import org.springframework.jdbc.core.RowMapper;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
 
 public class StudentRowMapper implements RowMapper<Student> {
     @Override
     public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+        Date date = rs.getDate("balance_defrost_date");
+        LocalDate localDate = null;
+
+        if (date != null)
+            localDate = date.toLocalDate();
+
         return new Student(
                 rs.getLong("d_uid"),
                 rs.getFloat("gold_balance"),
                 rs.getBoolean("is_engaged"),
-                rs.getDate("balance_defrost_date").toLocalDate()
+                localDate
         );
     }
 }
