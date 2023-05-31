2e676e965717146ff9278bbd0a475c4ac75e52c5
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-31 14:56:14 +0200

    update: added currency report messaging

--- src/main/java/com/learngerman/wizardbot/day_event/DayEvent.java
+++ src/main/java/com/learngerman/wizardbot/day_event/DayEvent.java
@@ -1,27 +1,27 @@
 package com.learngerman.wizardbot.day_event;
 
+import com.learngerman.wizardbot.currency.CurrencyReport;
 import com.learngerman.wizardbot.student.StudentService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Mono;
-
 import static com.learngerman.wizardbot.currency.GoldCurrency.GOLD_DAY_TAX;
+
 
 @Service
 public class DayEvent {
 
     private final StudentService studentService;
+    private final CurrencyReport currencyReport;
 
-    public DayEvent(StudentService studentService) {
+    public DayEvent(StudentService studentService, CurrencyReport currencyReport) {
         this.studentService = studentService;
+        this.currencyReport = currencyReport;
     }
 
-    @Scheduled(cron = "0 0 9 * * ?", zone="Europe/Berlin")
-    public Mono<Void>process() {
-        studentService.decreaseStudentsGoldCurrencyBy(GOLD_DAY_TAX);
-        //try
-        //send report about the students to set channel
-
-        return Mono.empty();
+    @Scheduled(cron = "0 * * * * *", zone="Europe/Berlin")
+    public void process() {
+        studentService.decreaseAllStudentsGoldCurrencyBy(GOLD_DAY_TAX);
+        currencyReport.send();
+        studentService.updateFreezeStatus();
     }
 }
