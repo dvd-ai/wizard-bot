cef9239f641c75efc93a5bf2868815b9d63f3af8
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-05-31 14:37:47 +0200

    added: doesNotHaveAuthor method

--- src/main/java/com/learngerman/wizardbot/event/MessageValidator.java
+++ src/main/java/com/learngerman/wizardbot/event/MessageValidator.java
@@ -17,6 +17,10 @@ public class MessageValidator {
         return content.startsWith(">");
     }
 
+    public boolean doesNotHaveAuthor(Message message) {
+        return message.getAuthor().isEmpty();
+    }
+
     public boolean isBot(Optional<User> author) {
         return author.get().isBot();
     }
