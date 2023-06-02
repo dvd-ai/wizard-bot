8f9a62c6098247b27c77915ae5cb142a55c1f972
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-06-02 07:40:45 +0200

    added: field 'is_in_guild'

--- src/main/resources/db/migration/V20230508153114__create_students_table.sql
+++ src/main/resources/db/migration/V20230508153114__create_students_table.sql
@@ -2,5 +2,6 @@ create table students (
    d_uid BIGINT PRIMARY KEY,
    gold_balance REAL,
    is_engaged BOOLEAN NOT NULL,
-   balance_defrost_date DATE
+   balance_defrost_date DATE,
+   is_in_guild BOOLEAN NOT NULL
 );
