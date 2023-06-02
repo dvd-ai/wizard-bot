fd2ecc58d0cef057ef87338f7ca9c512e23c9d61
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-06-02 08:06:54 +0200

    added: getStudentsInfosMapMono modified: getStudentsInfosListMono (naming)

--- src/main/java/com/learngerman/wizardbot/util/GuildUtil.java
+++ src/main/java/com/learngerman/wizardbot/util/GuildUtil.java
@@ -1,6 +1,7 @@
 package com.learngerman.wizardbot.util;
 
 import com.learngerman.wizardbot.command.MemberInfo;
+import com.learngerman.wizardbot.student.Student;
 import discord4j.common.util.Snowflake;
 import discord4j.core.GatewayDiscordClient;
 import discord4j.core.object.entity.Guild;
@@ -8,15 +9,18 @@ import discord4j.core.object.entity.channel.GuildChannel;
 import discord4j.rest.http.client.ClientException;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
 import java.util.List;
+import java.util.Map;
 
 public class GuildUtil {
 
     private GuildUtil() {
     }
 
-    public static Mono<List<MemberInfo>> getStudentsInfosMono(List<Long> studentIds, Mono<Guild> guildMono) {
+    public static Mono<List<MemberInfo>> getStudentsInfosListMono(List<Long> studentIds, Mono<Guild> guildMono) {
         return Flux.fromIterable(studentIds)
                 .map(Snowflake::of)
                 .flatMap(snowflake -> guildMono.flatMap(guild -> guild.getMemberById(snowflake)))
@@ -27,6 +31,21 @@ public class GuildUtil {
                         member.getAvatarUrl()
                 )).collectList();
     }
+
+    public static Mono<Map<Student, MemberInfo>> getStudentsInfosMapMono(List<Student> students, Mono<Guild> guildMono) {
+        return Flux.fromIterable(students)
+                .flatMap(student -> guildMono.flatMap(guild -> guild.getMemberById(Snowflake.of(student.getDiscordId()))
+                        .onErrorResume(ClientException.isStatusCode(404), e -> Mono.empty())
+                        .map(member -> Tuples.of(student, new MemberInfo(
+                                member.getUsername(),
+                                member.getDiscriminator(),
+                                member.getAvatarUrl()
+                        )))
+                ))
+                .collectMap(Tuple2::getT1, Tuple2::getT2);
+    }
+
+
 
     public static Mono<Guild> getGuildMono(Snowflake channelId, GatewayDiscordClient client) {
         return client.getChannelById(channelId)
