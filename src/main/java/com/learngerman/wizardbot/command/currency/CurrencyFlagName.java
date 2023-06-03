023db240bb2c10762ac53d48a936376970cdcac1
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-06-03 07:44:29 +0200

    constants for naming commands and getting their names

---
+++ src/main/java/com/learngerman/wizardbot/command/currency/CurrencyFlagName.java
@@ -0,0 +1,13 @@
+package com.learngerman.wizardbot.command.currency;
+
+public final class CurrencyFlagName {
+
+    private CurrencyFlagName() {
+    }
+
+    public static final String FREEZE_FLAG_NAME = "freeze";
+    public static final String UNFREEZE_FLAG_NAME = "unfreeze";
+    public static final String INFO_FLAG_NAME = "info";
+    public static final String GRANT_FLAG_NAME = "grant";
+    public static final String CONFISCATE_FLAG_NAME = "confiscate";
+}
