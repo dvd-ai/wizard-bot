7a61d54ca23c12d7bf07869977042eb14f99e442
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2023-06-07 09:32:16 +0200

    modified: GOLD_START_CAPITAL = 50

--- src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
+++ src/main/java/com/learngerman/wizardbot/currency/GoldCurrency.java
@@ -1,7 +1,7 @@
 package com.learngerman.wizardbot.currency;
 
 public class GoldCurrency {
-    public static final float GOLD_START_CAPITAL = 25f;
+    public static final float GOLD_START_CAPITAL = 50f;
     public static final float GOLD_MESSAGE_REWARD_PER_WORD = 0.1f;
     public static final float GOLD_VOICE_REWARD_PER_SECOND = 0.01f;
     public static final float GOLD_DAY_TAX = 10f;
