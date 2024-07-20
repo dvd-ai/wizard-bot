4bbff91d1d5b380287e75494faa15aa5fbd222a8
_Daniil_Denisov_ <daniildenisoww@gmail.com>
2024-07-20 07:58:51 +0200

    removed discriminator from MemberInfo POJO

--- src/main/java/com/learngerman/wizardbot/command/MemberInfo.java
+++ src/main/java/com/learngerman/wizardbot/command/MemberInfo.java
@@ -2,16 +2,13 @@ package com.learngerman.wizardbot.command;
 
 public class MemberInfo {
     private String username;
-    private String discriminator;
-
     private String avatar;
 
     public MemberInfo() {
     }
 
-    public MemberInfo(String username, String discriminator, String avatar) {
+    public MemberInfo(String username, String avatar) {
         this.username = username;
-        this.discriminator = discriminator;
         this.avatar = avatar;
     }
 
@@ -21,14 +18,6 @@ public class MemberInfo {
 
     public void setUsername(String username) {
         this.username = username;
-    }
-
-    public String getDiscriminator() {
-        return discriminator;
-    }
-
-    public void setDiscriminator(String discriminator) {
-        this.discriminator = discriminator;
     }
 
     public String getAvatar() {
